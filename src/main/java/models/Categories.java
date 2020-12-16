f81291eaa35cc8669b3742324c3f1f18e1a60b97
Lorenah <lorayne911@gmail.com>
2020-12-16 20:27:12 +0300

    added Categories class

--- src/main/java/models/Categories.java
+++ src/main/java/models/Categories.java
@@ -1,4 +1,53 @@
 package models;
 
+import java.util.Objects;
+
 public class Categories {
+    private int id;
+    private String name;
+    private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categories that = (Categories) o;
+        return id == that.id &&
+                name.equals(that.name) &&
+                description.equals(that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setBio(String bio) {
+        this.description = description;
+    }
+
+    public Categories(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
